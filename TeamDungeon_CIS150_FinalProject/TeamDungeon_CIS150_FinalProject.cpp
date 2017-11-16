4e3c6d57fb4a323106264f88c1dd165f266cbf8b
theadrick46 <33502101+theadrick46@users.noreply.github.com>
2017-11-15 19:02:05 -0500

    Tanness 1st commit
    I am commiting the start of my portion of the project.

--- TeamDungeon_CIS150_FinalProject/TeamDungeon_CIS150_FinalProject.cpp
+++ TeamDungeon_CIS150_FinalProject/TeamDungeon_CIS150_FinalProject.cpp
@@ -5,15 +5,41 @@
 //Hannah Seccia
 
 
-<< << << < HEAD
-=======
->>>>>>> f3fb0445cffaf372ee8ed9af4c035b599a254841
-
 #include "stdafx.h"
+#include <iostream>
+#include <string>
+using namespace std;
+
+int mainMenu(int choice)
+{
+	string name;
+	cout << "Enter your name: ";
+	cin >> name;
+	cout << "Welcome " << name << ". You are about to embark on a marvelous journey. It will be " << endl;
+	cout << "full of monsters and mayhem... and possibly treasure! Good luck on your travels!";
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
 
 
 int main()
 {
+	int choice; //menu choice
+	
+
+	mainMenu(choice);
 
 	system("pause");
     return 0;
