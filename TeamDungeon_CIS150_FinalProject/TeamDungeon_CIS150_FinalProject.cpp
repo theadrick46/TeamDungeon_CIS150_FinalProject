a217ebeb246c87500a516787d2a844b2d4627838
hseccia <33502113+hseccia@users.noreply.github.com>
2017-12-02 19:01:25 -0500

    Revert "Revert "Menu changed to Switch""
    This reverts commit e3bf12e84bc5628a9767855c119027a5ccfaf6f9.

--- TeamDungeon_CIS150_FinalProject/TeamDungeon_CIS150_FinalProject.cpp
+++ TeamDungeon_CIS150_FinalProject/TeamDungeon_CIS150_FinalProject.cpp
@@ -184,18 +184,10 @@ void admin(string password) //function that allows admin to pull up map *passwor
 		cout << "Please enter correct password: ";
 	}
 }
-<<<<<<< HEAD
 
 void mainGameMenu(int choice, int movement, string inv, int health, string goodbye, string password, double coordinatelevel[])
 {
 	cout << "What would you like to do?" << endl;//main menu         MAY NEED TO MOVE TO ANOTHER FUNCTION?
-=======
-																											
-void mainGameMenu(int health, string password, double coordinatelevel[], string person[], string floorCeiling, int levelSizes[], int levelCounter, string weapon[], int weaponCounter) 
-{
-
-	cout << "What would you like to do?" << endl;//main menu         
->>>>>>> parent of 9b19a68... Menu changed to Switch
 	cout << "1. Move" << endl;
 	cout << "2. Check Inventory" << endl;
 	cout << "3. Check Health" << endl;
@@ -208,20 +200,12 @@ void mainGameMenu(int health, string password, double coordinatelevel[], string 
 
 	if (choice == 1)
 	{
-<<<<<<< HEAD
 		askMove(movement, coordinatelevel); //allows user to move
-=======
-		askMove(coordinatelevel, levelSizes, levelCounter); //allows user to move
->>>>>>> parent of 9b19a68... Menu changed to Switch
 	}
 
 	else if (choice == 2)
 	{
-<<<<<<< HEAD
 		checkInv(inv); //allows user to check inventory
-=======
-		checkInv(weaponCounter, weapon); //allows user to check inventory
->>>>>>> parent of 9b19a68... Menu changed to Switch
 	}
 
 	else if (choice == 3)
@@ -231,11 +215,7 @@ void mainGameMenu(int health, string password, double coordinatelevel[], string 
 
 	else if (choice == 4)
 	{
-<<<<<<< HEAD
 		exit(goodbye); //allows user to exit
-=======
-		exit();
->>>>>>> parent of 9b19a68... Menu changed to Switch
 	}
 
 	else if (choice == 9)
